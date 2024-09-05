821db6210654d2433814fe2e3e3180e0af78781f
Yanzhen Huang <huangyanzhen0108@163.com>
2024-09-05 18:17:27 +0800

    :bee: Add reversed dictionary of keypoints-names. Add angle calculation for keypoints.

--- step01_annotate_image_mmpose/annotate_image.py
+++ step01_annotate_image_mmpose/annotate_image.py
@@ -9,11 +9,9 @@ import mimetypes
 import os
 import time
 from argparse import ArgumentParser
-
 from mmpose.apis import (
     inference_topdown,
-    init_model as init_pose_estimator
-)
+    init_model as init_pose_estimator)
 from mmpose.evaluation.functional import nms
 from mmpose.registry import VISUALIZERS
 from mmpose.structures import merge_data_samples, split_instances
@@ -21,106 +19,57 @@ from mmpose.utils import adapt_mmdet_pipeline, register_all_modules
 
 try:
     from mmdet.apis import inference_detector, init_detector
-
     has_mmdet = True
 except (ImportError, ModuleNotFoundError):
     has_mmdet = False
 
+# Local
+from keypoint_info import keypoint_indexes, keypoint_names
+
 register_all_modules()
 
-keypoint_names = {
-    0: 'Body-Chin',
-    1: 'Body-Left_eye',
-    2: 'Body-Right_eye',
-    3: 'Body-Left_ear',
-    4: 'Body-Right_ear',
-    5: 'Body-Left_shoulder',
-    6: 'Body-Right_shoulder',
-    7: 'Body-Left_elbow',
-    8: 'Body-Right_elbow',
-    9: 'Body-Left_wrist',
-    10: 'Body-Right_wrist',
-    11: 'Body-Left_hip',
-    12: 'Body-Right_hip',
-    13: 'Body-Left_knee',
-    14: 'Body-Right_knee',
-    15: 'Body-Left_ankle',
-    16: 'Body-Right_ankle',
-    17: 'Foot-Left_toe',
-    18: 'Foot-Left_pinky',
-    19: 'Foot-Left_heel',
-    20: 'Foot-Right_toe',
-    21: 'Foot-Right_pinky',
-    22: 'Foot-Right_heel',
-    23: 'Face-Right_hairroot',
-    24: 'Face-Right_zyngo',
-    25: 'Face-Right_face_top',
-    26: 'Face-Right_face_mid',
-    27: 'Face-Right_face_bottom',
-    28: 'Face-Right_chin_top',
-    29: 'Face-Right_chin_mid',
-    30: 'Face-Right_chin_bottom',
-    31: 'Face-Chin',
-    32: 'Face-Left_chin_bottom',
-    33: 'Face-Left_chin_mid',
-    34: 'Face-Left_chin_top',
-    35: 'Face-Left_face_bottom',
-    36: 'Face-Left_face_mid',
-    37: 'Face-Left_face_top',
-    38: 'Face-Left_zyngo',
-    39: 'Face-Left_hairroot',
-    40: 'Face-Right_eyebrow_out',
-    41: 'Face-Right_eyebrow_out_mid',
-    42: 'Face-Right_eyebrow_mid',
-    43: 'Face-Right_eyebrow_mid_in',
-    44: 'Face-Right_eyebrow_in',
-    45: 'Face-Left_eyebrow_in',
-    46: 'Face-Left_eyebrow_mid_in',
-    47: 'Face-Left_eyebrow_mid',
-    48: 'Face-Left_eyebrow_out_mid',
-    49: 'Face-Left_eyebrow_out',
-    50: 'Face-Nose_top',
-    51: 'Face-Nose_top_mid',
-    52: 'Face-Nose_bottom_mid',
-    53: 'Face-Nose_bottom',
-    54: 'Face-Right_nostril_out',
-    55: 'Face-Right_nostril_mid',
-    56: 'Face-Nostril',
-    57: 'Face-Left_nostril_mid',
-    58: 'Face-Left_nostril_out',
-    59: 'Face-Right_eye_out',
-    60: 'Face-Right_eye_up_out',
-    61: 'Face-Right_eye_up_in',
-    62: 'Face-Right_eye_in',
-    63: 'Face-Right_eye_down_in',
-    64: 'Face-Right_eye_down_out',
-    65: 'Face-Left_eye_in',
-    66: 'Face-Left_eye_up_in',
-    67: 'Face-Left_eye_up_out',
-    68: 'Face-Left_eye_out',
-    69: 'Face-Left_eye_down_out',
-    70: 'Face-Left_eye_down_in',
-    71: 'Face-Lips_l1_right_out',
-    72: 'Face-Lips_l1_right_mid',
-    73: 'Face-Lips_l1_right_in',
-    74: 'Face-Lips_l1_mid',
-    75: 'Face-Lips_l1_left_in',
-    76: 'Face-Lips_l1_left_mid',
-    77: 'Face-Lips_l1_left_out',
-    78: 'Face-Lips_l4_left_out',
-    79: 'Face-Lips_l4_left_in',
-    80: 'Face-Lips_l4_mid',
-    81: 'Face-Lips_l4_right_in',
-    82: 'Face-Lips_l4_right_out',
-    83: 'Face-Lips_l2_right_out',
-    84: 'Face-Lips_l2_right_in',
-    85: 'Face-Lips_l2_mid',
-    86: 'Face-Lips_l2_left_in',
-    87: 'Face-Lips_l2_left_out',
-    88: 'Face-Lips_l3_left',
-    89: 'Face-Lips_l3_mid',
-    90: 'Face-Lips_l3_right',
-}
+
+def _calc_angle(edge_points, mid_point):
+    """
+    Calculate the angle based on the given edge points and middle point.
+    :param edge_points: A tuple of two coordinates of the edge points of the angle.
+    :param mid_point: The coordinate of the middle point of the angle.
+    :return: The degree value of the angle.
+    """
+    # Left, Right
+    p1, p2 = [np.array(pt) for pt in edge_points]
+
+    # Mid
+    m = np.array(mid_point)
+
+    # Angle
+    radians = np.arctan2(p2[1] - m[1], p2[0] - m[0]) - np.arctan2(p1[1] - m[1], p1[0] - m[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+def calc_keypoint_angle(keypoints_one_person, edge_keypoints, mid_keypoint):
+    """
+    Calculate the angle using the given edge pionts and middle point by their names.
+    :param keypoints_one_person: The set of keypoints of a single person.
+    :param edge_keypoints: A tuple of the names of the two edge keypoints.
+    :param mid_keypoint: The name of the middle keypoint.
+    :return: The targeted angle.
+    """
+
+    # Names
+    n1, n2 = edge_keypoints
+    nm = mid_keypoint
+
+    # Coordinates
+    # Name --> [keypoint_indexes] --> index_number --> [keypoints_one_person] --> (x,y,score) --> [:2] --> (x,y)
+    coord1, coord2 = keypoints_one_person[keypoint_indexes[n1]][:2], keypoints_one_person[keypoint_indexes[n2]][:2]
+    coordm = keypoints_one_person[keypoint_indexes[nm]][:2]
+
+    return _calc_angle([coord1, coord2], keypoint_indexes[coordm])
 
 
 def process_one_image(img,
