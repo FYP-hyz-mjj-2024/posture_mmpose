c01245976d1ac06ae28106159ae6c094429442bb
Yanzhen Huang <huangyanzhen0108@163.com>
2024-10-03 16:17:24 +0800

    :bee: Improve [parse_file_name.py]: Move file name parsing feature to utils from annotate_image.py

--- step01_annotate_image_mmpose/annotate_image.py
+++ step01_annotate_image_mmpose/annotate_image.py
@@ -33,41 +33,10 @@ import config as cfg
 from keypoint_info import keypoint_indexes, keypoint_names
 from utils.calculations import calc_keypoint_angle
 from utils.opencv_utils import init_websocket, render_detection_rectangle, yield_video_feed
+from utils.parse_file_name import parseFileName
 from step02_train_model_cnn.train_model_hyz import MLP
 
 register_all_modules()
-
-
-def parseFileName(video_file_name: str, extension: str):
-    """
-    Parse file name to get the information about the file.
-    Information:
-
-    - capture_date: Date of capture.
-    - capture_time: Time of capture.
-    - human_model_name: Name of the human model.
-    - label: Label of the video. For example: "UN-Horiz-L" means "using phone horizontally with left hand".
-    - extensions: Extending postures. For example: "TC-Face-L" means "touching face with left hand".
-    - weight: The "Label" for regression, i.e. the confidence that the person is using a cellphone.
-
-    :param video_file_name: Name of the video file.
-    :return: An information dictionary of the video.
-    """
-    parsed = re.split(r'_', video_file_name.replace(extension, ""))
-    if len(parsed) != 7:
-        raise Exception("Video name is not valid.")
-
-    info = {
-        'capture_date': int(parsed[0]),
-        'capture_time': int(parsed[1]),
-        'human_model_name': str(parsed[2]),
-        'label': str(parsed[3]),
-        'extensions': str(parsed[4]),
-        'weight': float(parsed[5][:1] + "." + parsed[5][1:]),
-        'frame_number': int(parsed[6])
-    }
-    return info
-
 
 def processOneImage(img,
                     bbox_detector_model,
