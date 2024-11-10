77995f7fde05aea6d97770a97d4444017d857428
Yanzhen Huang <huangyanzhen0108@163.com>
2024-11-10 15:10:39 +0800

    :bee: Tidy codes in annotate_image.py, fix out-dated docstrings, polish ambiguous comments, fix improper local param names

--- step01_annotate_image_mmpose/annotate_image.py
+++ step01_annotate_image_mmpose/annotate_image.py
@@ -80,64 +80,42 @@ def getMMPoseEssentials(det_config: str, det_chkpt: str,
     return bbox_detector, pose_estimator, visualizer
 
 
-def renderTheResults(img: Union[str, np.ndarray],
-                     data_samples,
-                     estim_results_visualizer=None,
-                     show_interval=0.001):
-    # Render the results
-    if isinstance(img, str):
-        img = mmcv.imread(img, channel_order='rgb')
-    elif isinstance(img, np.ndarray):
-        img = mmcv.bgr2rgb(img)
-
-    if estim_results_visualizer is not None:
-        estim_results_visualizer.add_datasample(
-            'result',
-            img,
-            data_sample=data_samples,
-            draw_gt=False,
-            draw_heatmap=mcfg.draw_heatmap,
-            draw_bbox=mcfg.draw_bbox,
-            show_kpt_idx=mcfg.show_kpt_idx,
-            skeleton_style=mcfg.skeleton_style,
-            show=mcfg.show,
-            wait_time=show_interval,
-            kpt_thr=mcfg.kpt_thr)
-
-
 def processOneImage(img: Union[str, np.ndarray],
                     bbox_detector_model,
                     pose_estimator_model,
                     bbox_threshold=mcfg.bbox_thr_single) -> Tuple[ndarray, ndarray, PoseDataSample]:
     """
-    Given an image, first use bbox detection model to retrieve object boundary boxes.
-    Then, feed the sub images defined by the bbox into the pose estimation model to get key points.
-    Lastly, visualize predicted key points (and heatmaps) of one image.
+    This is the 1st information layer. This function gets the raw data from the image.
 
     data_samples --> pred_instances --> [keypoints, keypoint_scores, bboxes]
 
     :param img: The image.
     :param bbox_detector_model: The model to retrieve boundary boxes.
     :param pose_estimator_model: The model to estimate a person's pose, i.e., retrieve key points.
-    :param estim_results_visualizer: The result visualizer.
     :param bbox_threshold: The threshold of IoU where the boundary boxes will be recorded into the list.
-    :param show_interval: The wait time of the visualizer.
-    :return: Raw Results of prediction.
+
+    Returns:
+
+    - keypoints_list: 3-layered list. First layer: Num of people. Second layer: Num of keypoints. Third Layer: Num of
+    values in a keypoint, i.e., x, y and score.
+
+    - xyxy_list: 2-layered list. First layer: Num of people. Second layer: xmin, xmax, ymin, ymax of one bbox.
+
+    - data_samples: Raw data samples from mmpose for visualization.
     """
 
-    # Get boundary boxes xyxy list, only keep bboxes with high confidence.
+    # List of boundary boxes xyxy coordinates.
+    # Only keep bboxes above the bbox threshold.
     det_result = inference_detector(bbox_detector_model, img)
     pred_instance = det_result.pred_instances.cpu().numpy()
     bboxes = np.concatenate((pred_instance.bboxes, pred_instance.scores[:, None]), axis=1)
     bboxes = bboxes[np.logical_and(pred_instance.labels == mcfg.det_cat_id, pred_instance.scores > bbox_threshold)]
     bboxes = bboxes[nms(bboxes, mcfg.nms_thr), :4]
 
-    # Get key points list.
+    # A list of keypoint sets.
+    # A keypoint set is a list of keypoints.
     pose_results = inference_topdown(pose_estimator_model, img, bboxes)
     data_samples = merge_data_samples(pose_results)
-
-    # if there is no instance detected, return None
-    # return data_samples.get('pred_instances', None)
     raw_predictions = data_samples.get('pred_instances', None)
 
     # Keypoint coordinates --> (num_people, 17, 3)
@@ -187,7 +165,9 @@ def getOneFeatureRow(keypoints_list: ndarray,
                      detection_targets: Union[List, ndarray],
                      mode: str = 'hyz') -> Union[List, Tuple[ndarray, ndarray]]:
     """
-    Post process the raw features received from process_one_image.
+    This is the 2nd information layer. This function gets the raw data from the first information layer, and process it
+    into an array or cube.
+
     From the keypoints list, extract the most confident person in the image. Then, convert the
     keypoints list of this person into a flattened feature vector.
 
@@ -209,7 +189,8 @@ def getOneFeatureRow(keypoints_list: ndarray,
 
         # Shape=(2m)
         return kas_one_person
-    elif mode == 'mjj':
+
+    elif mode == 'mjj':  # TODO: Finish the new cube angle logic.
         _shape = detection_targets.shape
         angles = np.empty(shape=_shape)
         scores = np.empty(shape=_shape)
@@ -222,61 +203,37 @@ def getOneFeatureRow(keypoints_list: ndarray,
                                                                            detection_targets[i, j, k, 1])
 
 
-def processImagesInDir(img_dir: str,
-                       bbox_detector_model,
-                       pose_estimator_model,
-                       estim_results_visualizer=None,
-                       show_interval=0,
-                       detection_target_list=None,
-                       use_weight=False) -> np.ndarray:
+def renderTheResults(img: Union[str, np.ndarray],
+                     data_samples,
+                     estim_results_visualizer=None,
+                     show_interval=0.001):
     """
-    Batch annotate multiple images within a directory.
-    :param img_dir: Directory where multiple images are stored.
-    :param bbox_detector_model: MMPose boundary box detection model.
-    :param pose_estimator_model: MMPose estimator model.
-    :param estim_results_visualizer: MMPose estimation results visualizer.
-    :param show_interval: Interval among each image.
-    :param detection_target_list: List of detection target.
-    :param use_weight: Whether to use manual-given weight as label.
-    :return: Feature Matrix.
+    Render the results of inferences into the mmcv canvas.
+    :param img: The path of the image, or a video frame.
+    :param data_samples: The inference result from that current image.
+    :param estim_results_visualizer: The mmpose result visualizer object.
+    :param show_interval: Display interval.
+    :return:
     """
+    # Render the results
+    if isinstance(img, str):
+        img = mmcv.imread(img, channel_order='rgb')
+    elif isinstance(img, np.ndarray):
+        img = mmcv.bgr2rgb(img)
 
-    # Shape: (num_file=num_people, num_features)
-    # num_features = num_angles + num_scores = 2 * num_angles = 2 * num_scores
-    kas_multiple_images = []
-
-    for root, dirs, files in os.walk(img_dir):
-        # For multiple images, do:
-        for file in files:
-            if not file.endswith(".jpg"):
-                continue
-            file_path = os.path.join(root, file)
-
-            # Single Image, may be multiple person
-            keypoints_list, xyxy_list, data_samples = processOneImage(
-                file_path,
-                bbox_detector_model,
-                pose_estimator_model)
-
-            renderTheResults(file_path, data_samples, estim_results_visualizer, 0.001)
-
-            # A flattened angle-score vector of a single person.
-            one_row = getOneFeatureRow(keypoints_list[0], detection_target_list)
-
-            # Information of the image
-            img_info = parseFileName(file, ".jpg")
-            if use_weight:
-                if 'weight' not in img_info:
-                    raise Exception("You need to specify weight in the file name!")
-                one_row.append(img_info['weight'])
-
-            # Collect this person.
-            kas_multiple_images.append(one_row)
-
-    # Shape: (num_people, num_features)
-    feature_matrix = np.array(kas_multiple_images)
-
-    return feature_matrix
+    if estim_results_visualizer is not None:
+        estim_results_visualizer.add_datasample(
+            'result',
+            img,
+            data_sample=data_samples,
+            draw_gt=False,
+            draw_heatmap=mcfg.draw_heatmap,
+            draw_bbox=mcfg.draw_bbox,
+            show_kpt_idx=mcfg.show_kpt_idx,
+            skeleton_style=mcfg.skeleton_style,
+            show=mcfg.show,
+            wait_time=show_interval,
+            kpt_thr=mcfg.kpt_thr)
 
 
 def processVideosInDir(video_dir: str,
@@ -285,27 +242,39 @@ def processVideosInDir(video_dir: str,
                        detection_target_list: Union[List[List[Union[Tuple[str, str], str]]], ndarray],
                        skip_interval: int = 10,
                        mode: str = 'hyz') -> List[Dict[str, Union[str, np.ndarray]]]:
+    """
+    Convert all the videos gathered from a given directory.
+    "kas" = key-angle-score
+    :param video_dir: Directory to the video.
+    :param bbox_detector_model: MMPose boundary box detector model.
+    :param pose_estimator_model: MMPose pose estimation model.
+    :param detection_target_list: List of detection targets.
+    :param skip_interval: Interval between sampled frames.
+    :param mode: A single result to be an array or a cube.
+    :return:
+    """
     named_feature_matrices = []
 
-    for video_file in os.listdir(video_dir):
-        if not video_file.endswith('.mp4'):
+    for file_name in os.listdir(video_dir):
+
+        # Settle file sources
+        if not file_name.endswith('.mp4'):     # Skip all non-videos
             continue
-
-        kas_video = []
-        video_path = os.path.join(video_dir, video_file)
-        cap = cv2.VideoCapture(video_path)
-
+        video_path = os.path.join(video_dir, file_name)
         file_name_with_extension = os.path.basename(video_path)
         file_name_without_extension = os.path.splitext(file_name_with_extension)[0]
 
+        # Converting Process
+        cap = cv2.VideoCapture(video_path)
         if not cap.isOpened():
-            print(f"Cannot find {video_file}")
+            print(f"Cannot find {file_name}")
             continue
 
         cur_frame = 0
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        with tqdm(total=total_frames, desc=f"Processing {video_file}") as pbar:
+        kas_video = []
+        with tqdm(total=total_frames, desc=f"Processing {file_name}") as pbar:
             while True:
                 ret, frame = cap.read()
 
@@ -318,15 +287,10 @@ def processVideosInDir(video_dir: str,
                     continue
 
                 landmarks, _, data_samples = processOneImage(frame, bbox_detector_model, pose_estimator_model)
-                # renderTheResults(frame, data_samples, estim_results_visualizer=visualizer, show_interval=.001)
-                one_row = getOneFeatureRow(landmarks[0], detection_target_list) # TODO
+                one_row = getOneFeatureRow(landmarks[0], detection_target_list)
 
-                angle_cube, score_cube = getOneFeatureRow(landmarks[0], detection_target_list, mode)
-
-                img_info = parseFileName(file_name_without_extension + f"_{cur_frame}", ".mp4")
-                # if 'weight' not in img_info:
-                #     raise Exception("You need to specify weight in the file name!")
-                # one_row.append(img_info['weight'])
+                # TODO: Cube logic
+                # angle_cube, score_cube = getOneFeatureRow(landmarks[0], detection_target_list, mode)
 
                 kas_video.append(one_row)
 
@@ -338,7 +302,6 @@ def processVideosInDir(video_dir: str,
             named_feature_matrices.append({"name": file_name_without_extension,
                                            "feature_matrix": feature_matrix})
 
-            # saveFeatureMatToNPY(feature_matrix, save_path="../data/train/" + file_name_without_extension + ".npy")
     return named_feature_matrices
 
 
@@ -355,6 +318,8 @@ def saveFeatureMatToNPY(mat: np.ndarray, save_path: str) -> None:
 
 
 if __name__ == "__main__":
+    solution_mode = 'hyz'  # mjj
+    video_folder = "../data/blob/videos"
 
     # Initialize MMPose essentials
     detector, pose_estimator, visualizer = getMMPoseEssentials(
@@ -364,56 +329,15 @@ if __name__ == "__main__":
         pose_chkpt=mcfg.pose_checkpoint_train
     )
 
-    # List of detection targets
+    # Save the feature matrices.
+    named_feature_mats = processVideosInDir(video_dir=video_folder,
+                                            bbox_detector_model=detector,
+                                            pose_estimator_model=pose_estimator,
+                                            detection_target_list=kcfg.get_target_list(solution_mode),
+                                            skip_interval=10)
 
-    """
-    5. Image Processing
-    """
-    # input_type = 'video2hyz_npy'  # Alter this between 'image' and 'video'
-    input_type = 'video2mjj_npy'  #
-    overwrite = False
-
-    if input_type == 'image':
-
-        for root, dirs, files in os.walk("../data/train/img_from_video"):
-            for sub_dir in dirs:
-                print(f"Processing Images in: {sub_dir}...", end=" ")
-                if not overwrite and os.path.exists(f"../data/train/{sub_dir}.npy"):
-                    print(".npy file exists, deported.")
-                    continue
-                print("Saved.")
-
-                kas_multiple_images = processImagesInDir(
-                    img_dir=os.path.join(root, sub_dir),
-                    bbox_detector_model=detector,
-                    pose_estimator_model=pose_estimator,
-                    estim_results_visualizer=visualizer,
-                    detection_target_list=kcfg.get_target_list('hyz')
-                )
-                saveFeatureMatToNPY(kas_multiple_images, save_path=f"../data/train/{sub_dir}.npy")
-    elif input_type == 'video2hyz_npy':
-
-        video_folder = "../data/blob/videos"
-        named_feature_mats = processVideosInDir(video_dir=video_folder,
-                                                bbox_detector_model=detector,
-                                                pose_estimator_model=pose_estimator,
-                                                detection_target_list=kcfg.get_target_list('hyz'),
-                                                skip_interval=10)
-
-        [
-            saveFeatureMatToNPY(named_feature_mat['feature_matrix'],
-                                save_path="../data/train/" + named_feature_mat['name'] + ".npy")
-            for named_feature_mat in named_feature_mats
-        ]
-    elif input_type == 'video2mjj_npy':
-
-        video_folder = "../data/blob/videos"
-        named_feature_mats = processVideosInDir(video_dir=video_folder,
-                                                bbox_detector_model=detector,
-                                                pose_estimator_model=pose_estimator,
-                                                detection_target_list=kcfg.get_target_list('mjj'),
-                                                skip_interval=10)
-
-        for named_feature_mat in named_feature_mats:
-            saveFeatureMatToNPY(named_feature_mat['feature_matrix'],
-                                save_path="../data/train/" + named_feature_mat['name'] + ".npy")
+    [
+        saveFeatureMatToNPY(named_feature_mat['feature_matrix'],
+                            save_path="../data/train/" + named_feature_mat['name'] + ".npy")
+        for named_feature_mat in named_feature_mats
+    ]
