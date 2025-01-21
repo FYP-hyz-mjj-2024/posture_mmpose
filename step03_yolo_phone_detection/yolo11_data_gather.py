823f48727454bd85bc464a593b28621c0ca4b9dc
Yanzhen Huang <huangyanzhen0108@163.com>
2025-01-22 00:25:33 +0800

    :bee: Perfection on yolo dataset extrtaction.

--- step03_yolo_phone_detection/yolo11_data_gather.py
+++ step03_yolo_phone_detection/yolo11_data_gather.py
@@ -2,6 +2,7 @@
 import cv2
 import os
 import shutil
+from typing import Union, Dict
 
 # Utilities
 import numpy as np
@@ -15,6 +16,9 @@ from utils.opencv_utils import cropFrame
 
 
 def video_name2properties(video_name: str) -> int:
+    # TODO: I expanded this function's usages in getVideoProperties below.
+    #   The hashing performance has no need to worry.
+    #   The function will only be run once for each video.
     """
     Extract properties written in video's name.
     :param video_name: Video file name without extension.
@@ -28,11 +32,44 @@ def video_name2properties(video_name: str) -> int:
     return hand_idx
 
 
-def video2dataset(video_path, dataset_save_dir, step_size=10) -> None:
+def getVideoProperties(video_name: str, item=None) -> Union[Dict, int, str]:
+    """
+    Extract properties from a video's name. Available properties:
+    date, time, model name, position, green info, hand index, hand item.
+    The hand index and its corresponding chars are: L:0, R:1, B:2.
+    :param video_name: Name of the video file.
+    :param item: Which item of the properties to extract.
+    If not specified, the entire properties object will be given.
+    :return: Properties object or the required properties item.
+    """
+    if video_name.endswith(".mp4"):
+        video_name.replace(".mp4", "")
+
+    # Form properties object.
+    prop_dict_values = tuple(video_name.split("_"))
+    prop_dict_keys = ["date", "time", "model name", "position", "green info", "hand info"]
+    properties = {k: v for k, v in zip(prop_dict_keys, prop_dict_values)}
+
+    # Expand hand information.
+    properties["hand index"] = {"L": 0, "R": 1, "B": 2}[properties["hand info"][0]]
+    properties["hand item"] = properties["hand info"][1:]
+    del properties["hand info"]
+
+    if item is not None:
+        return properties[item]
+    else:
+        return properties
+
+
+def video2dataset(video_path: str,
+                  dataset_save_dir: str,
+                  mmpose_essentials,
+                  step_size: int = 10) -> None:
     """
     Save effective frames as input data from a video with mmpose and crop-frame methods.
     :param video_path: Path to the video .mp4 file.
     :param dataset_save_dir: Path to the directory where the dataset folders are saved.
+    :param mmpose_essentials: A tuple of bounding box detector and a pose estimator.
     :param step_size: Step size of frame sampling.
     :return: Nothing.
     """
@@ -45,49 +82,49 @@ def video2dataset(video_path, dataset_save_dir, step_size=10) -> None:
         raise IOError("Cannot configure cv2. Video might be corrupted, "
                       "or you may not have read permission of this file.")
 
+    # Set up datasets storage dir if necessary.
     os.makedirs(dataset_save_dir, exist_ok=True)
-
-    video_name = os.path.splitext(os.path.basename(video_path))[0]
-
+    video_name = os.path.splitext(os.path.basename(video_path))[0]  # No file ext.
     dataset_dir = os.path.join(dataset_save_dir, video_name)
-    if os.path.exists(dataset_dir):
-        shutil.rmtree(dataset_dir)
-    os.makedirs(dataset_dir, exist_ok=True) # prepare the output folder
 
-    which_hand = video_name2properties(video_name)
+    # Set up dir for this dataset.
+    if os.path.exists(dataset_dir):
+        shutil.rmtree(dataset_dir)  # Overwrite previous
+    os.makedirs(dataset_dir, exist_ok=True)
+
+    # Hand index.
+    which_hand = getVideoProperties(video_name, item="hand index")
     """
-    one single char, only can be 'L', 'R', 'B'. (left, right, both)
+    Index within range [0, 1, 2]. Corresponding to char as ['L', 'R', 'B'].
     """
     if which_hand < 0 or which_hand > 2:
         raise ValueError(f"{which_hand} is not in ['L', 'R', 'B']")
 
     # mmpose
-    bbox_detector, pose_estimator, _ = getMMPoseEssentials(
-        det_config=mcfg.det_config_train,
-        det_chkpt=mcfg.det_checkpoint_train,
-        pose_config=mcfg.pose_config_train,
-        pose_chkpt=mcfg.pose_checkpoint_train)
+    bbox_detector, pose_estimator = mmpose_essentials
 
-    frame_count = 0
-    stored_frames = []
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # Extract frames.
+    frame_count = 0     # Num. of frames the loop has covered.
+    total_frame_amount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    stored_frames = []  # Num of frames recorded.
 
-    with tqdm(total=total_frames, desc=f"Processing {video_path}") as pbar:
+    with tqdm(total=total_frame_amount, desc=f"Processing {video_path}") as pbar:
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret or cv2.waitKey(5) & 0xFF == 27:
                 break
 
+            # Extract the first person.
             keypoints_list, xyxy_list, _ = processOneImage(frame, bbox_detector, pose_estimator)
-
             keypoints, xyxy = keypoints_list[0], xyxy_list[0]
 
+            # Hand frame shape.
             bbox_w, bbox_h = xyxy[2] - xyxy[0], xyxy[3] - xyxy[1]
             hand_hw = (int(bbox_w * 0.7), int(bbox_w * 0.7))
             """Height and width (sequence matter) of the bounding box."""
 
+            # Hand frame centers.
             lhand_center, rhand_center = keypoints[9][:2], keypoints[10][:2]
-
             l_arm_vect, r_arm_vect = keypoints[9][:2] - keypoints[7][:2], keypoints[10][:2] - keypoints[8][:2]
             lhand_center += l_arm_vect * 0.8
             rhand_center += r_arm_vect * 0.8
@@ -111,13 +148,43 @@ def video2dataset(video_path, dataset_save_dir, step_size=10) -> None:
 
     # save
     cap.release()
-    for i, (frame, _) in tqdm(enumerate(stored_frames)):
+    for i, (frame, _) in tqdm(enumerate(stored_frames), desc=f"Saving {video_path}"):
         image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         resized_image = image.resize((64, 64))
-        resized_image.save(os.path.join(dataset_dir, f"_fig{i}.jpg"))
+        resized_image.save(os.path.join(str(dataset_dir), f"_fig{i}.jpg"))
+
+
+def videos2datasets(videos_save_dir: str, dataset_save_dir: str, sample_step_size=10):
+    """
+    Batch converting videos in a directory into datasets.
+    :param videos_save_dir: Directory where videos are stored.
+    :param dataset_save_dir: Directory where dataset folder is stored.
+    :param sample_step_size: Step size for frame extraction.
+    :return: Nothing.
+    """
+
+    # Initialize MMPose essentials.
+    bbox_detector, pose_estimator, _ = getMMPoseEssentials(
+        det_config=mcfg.det_config_train,
+        det_chkpt=mcfg.det_checkpoint_train,
+        pose_config=mcfg.pose_config_train,
+        pose_chkpt=mcfg.pose_checkpoint_train)
+
+    # Extract all videos.
+    for root, dirs, files in os.walk(videos_save_dir):
+        for file in files:
+            if not file.endswith('.mp4'):
+                print(f"Ignoring file {file}: Not an acceptable video file.")
+                continue
+            file_path = os.path.join(root, file)
+            print(f"Reading file {file}: Formulating dataset.")
+            video2dataset(video_path=file_path,
+                          dataset_save_dir=dataset_save_dir,
+                          mmpose_essentials=(bbox_detector, pose_estimator),
+                          step_size=sample_step_size)
 
 
 if __name__ == "__main__":
-    video2dataset(video_path="../data/blob/yolo_videos/20250121_2211_mjj_neck_nongreen_L000.mp4",
-                  dataset_save_dir="../data/yolo_dataset",
-                  step_size=10)
+    videos2datasets(videos_save_dir="../data/blob/yolo_videos/",
+                    dataset_save_dir="../data/yolo_dataset",
+                    sample_step_size=5)
