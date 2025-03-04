9f09df96d7aae27177af3b826b54c086bc83fbd3
Yanzhen Huang <huangyanzhen0108@163.com>
2025-03-04 15:18:37 +0800

    :bee: Add documentation for Focal Loss, L2 Regularzation and MCLoss.

--- step02_train_model_cnn/train_model.py
+++ step02_train_model_cnn/train_model.py
@@ -170,6 +170,17 @@ def train_and_evaluate(model,
 
 
 class ResPool3d(nn.Module):
+    """
+    Residual Pooling. Uses max-pooling to get max voxels in each local chunk.
+    Then, instead of leaving others out, ResPool3d adds the max-pooled voxels
+    back to the original 3d channel.
+    :param kernel_size: Size of the chunk kernel of max-pool.
+    :param stride: Number of steps to jump over when max-pool kernel run through.
+    :param padding: Edge padding of each channel.
+    :param dilation: Dilation.  (Doesn't matter here.)
+    :param ceil_mode: Ceiling mode. (Doesn't matter here.)
+    """
+
     __constants__ = ['kernel_size', 'stride', 'padding', 'dilation', 'ceil_mode']
     ceil_mode: bool
 
@@ -187,9 +198,10 @@ class ResPool3d(nn.Module):
         self.ceil_mode = ceil_mode
 
     def forward(self, input: Tensor):
-        _max_pooled, indices = F.max_pool3d(input, self.kernel_size, self.stride,
-                                       self.padding, self.dilation, ceil_mode=self.ceil_mode,
-                                       return_indices=True)
+        _max_pooled, indices = F.max_pool3d(input, self.kernel_size,
+                                            self.stride, self.padding,
+                                            self.dilation, ceil_mode=self.ceil_mode,
+                                            return_indices=True)
 
         output_shape = input.shape
 
@@ -202,17 +214,28 @@ class ResPool3d(nn.Module):
         # Add the emphasized values into the original input tensor.
         return input + output
 
+
 class FocalLoss(nn.Module):
+    """
+    Alpha-balanced variant of focal loss of one epoch.
+    Reference: https://arxiv.org/pdf/1708.02002
+    :param alpha: Linear parameter.
+    :param gamma: Exponent parameter.
+    :param reduction: Reduction method: mean or sum of all samples in this batch.
+    """
     def __init__(self, alpha=0.25, gamma=2, reduction='mean'):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.reduction = reduction
 
+        if self.reduction not in ['mean', 'sum']:
+            raise ValueError("Reduction method must be 'mean' or 'sum'.")
+
     def forward(self, inputs, labels):
-        probs = torch.softmax(inputs, dim=-1) # get softmax of 2 classes
-        pt = probs.gather(dim=-1, index=labels.unsqueeze(1)) # get p_t of observed class
-        loss = self.alpha * (1 - pt) ** self.gamma * -torch.log(pt) # calculate focal loss
+        probs = torch.softmax(inputs, dim=-1)                           # get softmax of 2 classes
+        pt = probs.gather(dim=-1, index=labels.unsqueeze(1))            # get p_t of groud-truth label class
+        loss = (self.alpha * ((1 - pt) ** self.gamma)) * (-torch.log(pt))     # calculate focal loss
 
         if self.reduction == 'mean':
             return loss.mean()
@@ -221,42 +244,57 @@ class FocalLoss(nn.Module):
         else:
             return loss
 
+
 class L2Regularization(nn.Module):
+    """
+    L2 Regularization. Prevents gradient explosion.
+    :param l2_lambda: Lambda of the L2 regularization, penalize factor.
+    """
     def __init__(self, l2_lambda=0.0001):
         super(L2Regularization, self).__init__()
         self.l2_lambda = l2_lambda
 
     def forward(self, _model):
         _device = next(_model.parameters()).device
+
+        # Loss: Square sum of all weights.
         l2_reg = torch.tensor(0., device=_device)
         for param in _model.parameters():
             l2_reg += torch.sum(param ** 2)
 
         return self.l2_lambda * l2_reg
 
-class WeightedLoss(nn.Module): # TODO: rename the class,  2024-2-28 18:21
-    def __init__(self, w1=0.6, w2=0.3, w3=0.1, focal_alpha = 0.25, focal_gamma=2, l2_lambda=0.0001):
-        super(WeightedLoss, self).__init__()
+
+class MCLoss(nn.Module):
+    """
+    MCLoss stands for Mean Cross-entropy loss. It is a weighted combination of
+    BCE Loss, Focal Loss and L2 Regularization.
+    """
+    def __init__(self, w1=0.6, w2=0.3, w3=0.1, focal_alpha=0.25, focal_gamma=2, l2_lambda=0.0001):
+        super(MCLoss, self).__init__()
         self.w1 = w1
         self.w2 = w2
         self.w3 = w3
         self.focal = FocalLoss(focal_alpha, focal_gamma)
         self.l2 = L2Regularization(l2_lambda)
 
-    def forward(self, outputs, labels, classifier3d):
+    def forward(self, outputs, labels, mlp3d_instance):
         # BCE with logits
-        unsoftmax_pt = outputs.gather(dim=-1, index=labels.unsqueeze(1)) # get p_t of observed class
-        input_ = unsoftmax_pt.squeeze(1)
-        target_ = labels.to(torch.float32)
+        unsoftmax_pt = outputs.gather(dim=-1, index=labels.unsqueeze(1))    # (batch_size=128, 1)
+        input_ = unsoftmax_pt.squeeze(1)        # (batch_size=128, ) array of ground-truth label prob.
+        target_ = labels.to(torch.float32)      # Ground-truth labels
         bce_loss = F.binary_cross_entropy_with_logits(input_, target_)
+
         # Focal Loss
         focal_loss = self.focal(outputs, labels)
+
         # L2 Regularization
-        l2_reg = self.l2(classifier3d)
+        l2_reg = self.l2(mlp3d_instance)
 
         # Total Loss
         total_loss = self.w1 * bce_loss + self.w2 * focal_loss + self.w3 * l2_reg
         return total_loss
+
 
 class MLP3d(nn.Module):
     def __init__(self, input_channel_num, output_class_num):
@@ -397,7 +435,7 @@ if __name__ == '__main__':  # TODO: compatible with mode 'mjj'
 
     model = MLP3d(input_channel_num=2, output_class_num=2).to(device)
     # criterion = nn.CrossEntropyLoss()  # Binary cross entropy loss
-    criterion = WeightedLoss(0.6, 0.3, 0.1)  # Binary cross entropy loss
+    criterion = MCLoss(0.6, 0.3, 0.1)  # Binary cross entropy loss
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)  # Auto adjust lr prevent o.f.
 
     report_loss = []
